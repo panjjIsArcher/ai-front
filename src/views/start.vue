7b1232c67e84b8733f7536aa7b487ca36eb43bb1
panjiajie <251907882@qq.com>
2022-11-23 10:45:43 +0800

    style: 背景色

--- src/views/start.vue
+++ src/views/start.vue
@@ -1,8 +1,11 @@
 <template>
-  <div class="layer">开始</div>
+  <div class="layer container">开始</div>
 </template>
 
 <script>
 export default {};
 </script>
-<style lang="less" scoped></style>
+<style lang="less" scoped>
+.container{
+    background-color: #f3f3f3;
+}</style>
